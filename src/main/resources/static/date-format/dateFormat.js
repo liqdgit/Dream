fd5407a8ec66e546021e73e784f40136a2e2f9a4
liqingdong <liqd163@163.com>
2017-10-20 11:08:54 +0800

    js时间格式化

--- src/main/resources/static/date-format/dateFormat.js
+++ src/main/resources/static/date-format/dateFormat.js
@@ -1,20 +1,32 @@
 /**
  * Created by liqd on 2017/10/20.
  */
-
+/**
+ * 时间格式化
+ * @param fmt 格式
+ * @returns {*}
+ * @constructor
+ */
 Date.prototype.Format = function (fmt) {
     var o = {
-        "y+": this.getYear(),
-        "M+": this.getMonth() + 1, //月份
-        "d+": this.getDate(), //日
-        "h+": this.getHours(), //小时
-        "m+": this.getMinutes(), //分
-        "s+": this.getSeconds(), //秒
-        "q+": Math.floor((this.getMonth() + 3) / 3), //季度
-        "S": this.getMilliseconds() //毫秒
+        "y+" : this.getYear(),
+        "M+" : this.getMonth() + 1, //月份
+        "d+" : this.getDate(), //日
+        "h+" : this.getHours(), //小时
+        "m+" : this.getMinutes(), //分
+        "s+" : this.getSeconds(), //秒
+        "q+" : Math.floor((this.getMonth() + 3) / 3), //季度
+        "S"  : this.getMilliseconds() //毫秒
     };
-    if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
-    for (var k in o)
-        if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+
+    if (/(y+)/.test(fmt)) {
+        fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+    }
+
+    for (var k in o) {
+        if (new RegExp("(" + k + ")").test(fmt)) {
+            fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+        }
+    }
     return fmt;
 }
