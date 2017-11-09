ec42e15e8d0ffbc98ef401786edcc4b13b225261
liqingdong <liqd163@163.com>
2017-11-09 17:33:56 +0800

    闲的无聊，改改代码

--- src/main/java/utils/ObjectSerializeUtil.java
+++ src/main/java/utils/ObjectSerializeUtil.java
@@ -7,7 +7,7 @@ import java.io.*;
  * <p>Description 对象序列化 </p>
  * <p>Company:    李清栋 </p>
  *
- * @Author         <a href="liqingdong"/>李清栋</a>
+ * @author         <a href="liqingdong"/>李清栋</a>
  * @CreateDate     2017/9/29 10:56
  */
 public class ObjectSerializeUtil {
@@ -22,8 +22,9 @@ public class ObjectSerializeUtil {
 	 * @return
 	 */
 	public static <T extends Serializable> byte[] serialize(T value) throws IOException {
-		if(value == null)
-			throw new NullPointerException("Can't serialize null"); 
+		if(value == null) {
+			throw new NullPointerException("Can't serialize null");
+		}
 		byte[] result;
 		ByteArrayOutputStream byteOutPut = new ByteArrayOutputStream();
 		ObjectOutputStream objOutPut = new ObjectOutputStream(byteOutPut);
@@ -33,21 +34,22 @@ public class ObjectSerializeUtil {
 		objOutPut.close();
 
 		result = byteOutPut.toByteArray();
-		return result;
-	}
+			return result;
+		}
 
-	/**
-	 * <p>Title:      反序列化. </p>
-	 * <p>Description </p>
-	 *
-	 * @param
-	 * @Author        <a href="liqingdong"/>李清栋</a>
-	 * @CreateDate    2017/9/29 10:58
-	 * @return
-	 */
+		/**
+		 * <p>Title:      反序列化. </p>
+		 * <p>Description </p>
+		 *
+		 * @param
+		 * @Author        <a href="liqingdong"/>李清栋</a>
+		 * @CreateDate    2017/9/29 10:58
+		 * @return
+		 */
 	public static <T extends Serializable> T deserialize(byte[] values) throws IOException, ClassNotFoundException {
-		if(values == null)
-			throw new NullPointerException("Can't serialize null"); 
+		if(values == null) {
+			throw new NullPointerException("Can't serialize null");
+		}
 		
 		ByteArrayInputStream byteInput = new ByteArrayInputStream(values);
 		ObjectInputStream objInput = new ObjectInputStream(byteInput);
